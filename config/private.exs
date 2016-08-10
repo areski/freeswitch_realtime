10bd3573263c727c4e4564ea2f4f7e03b612da37
areski <areski@gmail.com>
2016-08-10 17:27:36 +0200

    restructure

---
+++ config/private.exs
@@ -0,0 +1,44 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+# This configuration is loaded before any dependency and is restricted
+# to this project. If another project depends on this project, this
+# file won't be loaded nor affect the parent project. For this reason,
+# if you want to provide default values for your application for
+# 3rd-party users, it should be done in your "mix.exs" file.
+
+# You can configure for your application as:
+#
+#     config :fs_channels, key: :value
+#
+# And access this configuration in your application as:
+#
+#     Application.get_env(:fs_channels, :key)
+#
+# Or configure a 3rd-party app:
+#
+#     config :logger, level: :info
+#
+
+# It is also possible to import configuration files, relative to this
+# directory. For example, you can emulate configuration per environment
+# by uncommenting the line below and defining dev.exs, test.exs and such.
+# Configuration from the imported file will override the ones defined
+# here (which is why it is important to import them last).
+#
+#     import_config "#{Mix.env}.exs"
+
+config :fs_channels,
+  sqlite_db: "/home/areski/private/bitbucket/newfies-dialer/playground/channels/coredb.sqlite",
+  # sqlite_db: "/bam/coredb.sqlite",
+  influxdatabase:  "newfiesdialer"
+
+config :influxcon_app, FsChannels.InConnection,
+  host:      "162.243.160.14",
+  # http_opts: [ insecure: true, proxy: "http://company.proxy" ],
+  pool:      [ max_overflow: 0, size: 1 ],
+  port:      8086,
+  scheme:    "http",
+  writer:    Instream.Writer.Line
+
