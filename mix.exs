b16819ad2ed13895b5caa3cb011b0e8ea15ca615
areski <areski@gmail.com>
2016-08-10 19:08:19 +0200

    add exrm to package

--- mix.exs
+++ mix.exs
@@ -33,7 +33,10 @@ defmodule FsChannels.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:sqlitex, "~> 1.0.0"},
+    [{:ex_doc, "~> 0.13.0", only: :dev},
+     {:exrm, "~> 1.0.8"},
+     {:exrm_deb, "~> 0.0.6"},
+     {:sqlitex, "~> 1.0.0"},
      {:logger_file_backend, "0.0.7"},
      {:instream, "~> 0.12"}]
   end
@@ -41,8 +44,17 @@ defmodule FsChannels.Mixfile do
   defp description, do: "Collect and push channels information from FreeSWITCH Sqlite to InfluxDB"
 
   defp package do
-    [maintainers: ["Areski Belaid"],
+    [
+      name: :fs_channels_influxdb,
+      license_file: "LICENSE",
+      external_dependencies: [],
+      maintainers: ["Areski Belaid"],
+      vendor: "Areski Belaid",
       licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/areski/fs_channels_influxdb"}]
+      links: %{
+        "Github" => "https://github.com/areski/fs_channels_influxdb",
+        "Homepage" => "https://github.com/areski/fs_channels_influxdb"
+      }
+    ]
   end
 end
