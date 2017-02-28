71f47eaa4d069d749af64bb240d85af45bfc4edb
areski <areski@gmail.com>
2017-02-28 16:07:11 +0100

    [fix](mix) bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FreeswitchRealtime.Mixfile do
 
   def project do
     [app: :freeswitch_realtime,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.4.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
