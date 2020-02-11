df47a0ace154ab5c0915ac141dfc48f990433d8b
areski <areski@gmail.com>
2020-02-11 17:59:04 +0100

    [rel] bump version 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FSRealtime.Mixfile do
   def project do
     [
       app: :fs_realtime,
-      version: "1.0.0",
+      version: "1.0.2",
       elixir: "> 1.9.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
